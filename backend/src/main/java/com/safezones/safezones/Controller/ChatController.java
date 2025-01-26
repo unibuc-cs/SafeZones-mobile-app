3b6d418316edeb980d416cfbc0cde2d78a1f59c8
crazvan6 <razvancutuliga1@gmail.com>
2025-01-26 16:44:37 +0200

    Implemented Endpoints for Chat Functionality

--- backend/src/main/java/com/safezones/safezones/Controller/ChatController.java
+++ backend/src/main/java/com/safezones/safezones/Controller/ChatController.java
@@ -1,41 +1,74 @@
 package com.safezones.safezones.Controller;
 
-import com.safezones.safezones.Model.ChatMessage;
+import com.safezones.safezones.Dto.MessageDTO;
+import com.safezones.safezones.Dto.PointRequest;
+import com.safezones.safezones.Model.Message;
+import com.safezones.safezones.Model.MessageType;
+import com.safezones.safezones.Model.Point;
+import com.safezones.safezones.Model.User;
+import com.safezones.safezones.Repository.MessageRepository;
+import com.safezones.safezones.Repository.PointRepository;
+import com.safezones.safezones.Repository.UserRepository;
+import com.safezones.safezones.Service.WebSocketSessionManager;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/chat")
 public class ChatController {
 
-    @MessageMapping("/chat/{id}")
-    @SendTo("/topic/public/{id}")
-    public ChatMessage sendMessage(
-            @Payload ChatMessage chatMessage
-    ) {
-        return chatMessage;
+    private final WebSocketSessionManager sessionManager;
+    @Autowired
+    private MessageRepository chatRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewMessage(@RequestBody MessageDTO messageDto) {
+        Message newMessage = new Message(
+                messageDto.getContent(),
+                messageDto.getUserId(),
+                messageDto.getChatId(),
+                LocalDateTime.now()
+                //MessageType.CHAT
+        );
+        chatRepository.save(newMessage);
+        return "Message added successfully";
     }
 
-    @MessageMapping("/chat/addUser/{id}")
-    @SendTo("/topic/public/{id}")
-    public ChatMessage addUser(
-            @Payload ChatMessage chatMessage,
-            SimpMessageHeaderAccessor headerAccessor) {
+    @GetMapping(path = "/get/{chatId}")
+    public ResponseEntity<List<Map<String, String>>> getMessages(@PathVariable long chatId) {
+        List<Message> messages = chatRepository.findMessagesByChatId(chatId);
 
-        // Verifică dacă chatId și username sunt deja setate corect
-        if (headerAccessor.getSessionAttributes().containsKey("chatId")) {
-            log.info("User already added to chat: {}", chatMessage.getSender());
-        } else {
-            // Adăugăm utilizatorul la sesiune
-            headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-            headerAccessor.getSessionAttributes().put("chatId", chatMessage.getChatId());
+
+        List<Map<String, String>> responseMessages = new ArrayList<>();
+        for (Message message : messages) {
+            Map<String, String> messageWithUsername = new HashMap<>();
+            messageWithUsername.put("content", message.getContent());
+
+            User user = userRepository.findById(message.getUserId()).orElseThrow(() -> new RuntimeException("User not found"));
+            messageWithUsername.put("username", user.getUsername());
+            messageWithUsername.put("timestamp", message.getTimestamp().toString());
+
+            responseMessages.add(messageWithUsername);
         }
-        return chatMessage;
+
+        return ResponseEntity.ok(responseMessages);
     }
 }
-
-
